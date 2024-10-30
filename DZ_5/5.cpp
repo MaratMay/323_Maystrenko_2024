aae0da48e8629cd51320a0469c78bbd385eeb282
Marat Maystrenko <may.marat@yandex.ru>
2024-10-30 15:59:44 +0300

    Update 5.cpp

--- DZ_5/5.cpp
+++ DZ_5/5.cpp
@@ -18,19 +18,25 @@ int main() {
     std::cout << "Введите количество нитей" << std::endl;
     std::cin >> NUM_THREADS;
 
+    std::random_device generator;
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+    int size_array = NUM_THREADS * (b - a) * (b - a);
+    double* go_random = new double[size_array];
+    for (i = 0; i < size_array; i++)
+        go_random[i] = distribution(generator);
+
     auto start = std::chrono::system_clock::now();
 
-    #pragma omp parallel num_threads(NUM_THREADS) default(none) shared(N, a, b, x, p, NUM_THREADS) reduction(+:hits_b)
+    #pragma omp parallel num_threads(NUM_THREADS) default(none) shared(N, a, b, x, p, go_random, size_array, NUM_THREADS) reduction(+:hits_b)
     {
         #pragma omp for private(x_tmp, stepCount)
         for (i = 0; i < N; ++i) {
-            std::random_device generator;
-            std::uniform_real_distribution<double> distribution(0.0, 1.0);
             x_tmp = x;
             stepCount = 0;
             while (x_tmp != a && x_tmp != b && stepCount <= MAX_STEPS) {
                 stepCount++;
-                if (distribution(generator) > p)
+                if (go_random[i * (1 + omp_get_thread_num()) % size_array] > p)
                     x_tmp += 1;
                 else
                     x_tmp -= 1;
@@ -47,5 +53,6 @@ int main() {
     std::cout << "Basic time: " << elapsed.count() << " ms" << std::endl;
     std::cout << "Time per dot: " << elapsed.count() / N << " ms" << std::endl;
 
+    delete[] go_random;
     return 0;
 }
