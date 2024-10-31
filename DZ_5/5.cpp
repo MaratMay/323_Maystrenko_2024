74c871cf4b1469efccbcc8262aafd3db93c9ad6a
Marat Maystrenko <may.marat@yandex.ru>
2024-10-31 13:09:52 +0300

    Update 5.cpp

--- DZ_5/5.cpp
+++ DZ_5/5.cpp
@@ -2,9 +2,6 @@
 #include <random>
 #include <chrono>
 #include <omp.h>
-
-#define MAX_STEPS 100000
-
 int main() {
     int a, b, x, x_tmp, N, i, hits_b = 0, stepCount, sum_stepCount = 0, NUM_THREADS;
     double p;
@@ -20,19 +17,21 @@ int main() {
 
     int glob_time = time(NULL);
 
-    #pragma omp parallel num_threads(NUM_THREADS) default(none) shared(N, a, b, x, p, glob_time) reduction(+:hits_b) reduction(+:sum_stepCount)
+    #pragma omp parallel num_threads(NUM_THREADS) default(none) shared(N, a, b, x, p, glob_time, std::cout) reduction(+:hits_b) reduction(+:sum_stepCount)
     {
-        int my_id = omp_get_thread_num();
-        std::mt19937 my_gen(glob_time * my_id);
+        int my_id = omp_get_thread_num() + 1;
+        std::cout << my_id << std::endl;
+        std::mt19937 my_gen(glob_time * my_id * my_id * my_id * my_id * my_id);
         std::uniform_real_distribution<double> dis(0.0, 1.0);
         
         #pragma omp for private(x_tmp, stepCount)
         for (i = 0; i < N; ++i) {
             x_tmp = x;
             stepCount = 0;
-            while (x_tmp != a && x_tmp != b && stepCount <= MAX_STEPS) {
+            while (x_tmp != a && x_tmp != b) {
                 stepCount++;
-                x_tmp += dis(my_gen) > p ? 1 : -1;
+                if (dis(my_gen) > p) x_tmp += 1;
+                else x_tmp -= 1;
             }
             if (x_tmp == b) hits_b++;
             sum_stepCount += stepCount;
