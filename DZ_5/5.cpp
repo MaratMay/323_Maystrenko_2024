4363b7936983d690a6f84075bf8206306f14e4b2
Marat Maystrenko <may.marat@yandex.ru>
2024-10-31 12:22:35 +0300

    Update 5.cpp

--- DZ_5/5.cpp
+++ DZ_5/5.cpp
@@ -3,10 +3,10 @@
 #include <chrono>
 #include <omp.h>
 
-#define MAX_STEPS 10000
+#define MAX_STEPS 100000
 
 int main() {
-    int a, b, x, x_tmp, N, i, hits_b = 0, stepCount, NUM_THREADS;
+    int a, b, x, x_tmp, N, i, hits_b = 0, stepCount, sum_stepCount = 0, NUM_THREADS;
     double p;
     
     std::cout << "Введите начальную и конечную точку" << std::endl;
@@ -18,41 +18,29 @@ int main() {
     std::cout << "Введите количество нитей" << std::endl;
     std::cin >> NUM_THREADS;
 
-    std::random_device generator;
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    int glob_time = time(NULL);
 
-    int size_array = NUM_THREADS * (b - a) * (b - a);
-    double* go_random = new double[size_array];
-    for (i = 0; i < size_array; i++)
-        go_random[i] = distribution(generator);
-
-    auto start = std::chrono::system_clock::now();
-
-    #pragma omp parallel num_threads(NUM_THREADS) default(none) shared(N, a, b, x, p, go_random, size_array, NUM_THREADS) reduction(+:hits_b)
+    #pragma omp parallel num_threads(NUM_THREADS) default(none) shared(N, a, b, x, p, glob_time) reduction(+:hits_b) reduction(+:sum_stepCount)
     {
+        int my_id = omp_get_thread_num();
+        std::mt19937 my_gen(glob_time * my_id);
+        std::uniform_real_distribution<double> dis(0.0, 1.0);
+        
         #pragma omp for private(x_tmp, stepCount)
         for (i = 0; i < N; ++i) {
             x_tmp = x;
             stepCount = 0;
             while (x_tmp != a && x_tmp != b && stepCount <= MAX_STEPS) {
                 stepCount++;
-                if (go_random[i * (1 + omp_get_thread_num()) % size_array] > p)
-                    x_tmp += 1;
-                else
-                    x_tmp -= 1;
+                x_tmp += dis(my_gen) > p ? 1 : -1;
             }
-            if (x_tmp == b)
-                hits_b++;
+            if (x_tmp == b) hits_b++;
+            sum_stepCount += stepCount;
         }
     }
 
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double, std::milli> elapsed = end - start;
+    std::cout << "Вероятность достижения b: " << (double) hits_b / (double) N << std::endl;
+    std::cout << "Среднее время жизни частицы: " << sum_stepCount / N << std::endl;
 
-    std::cout << "Probability of reaching b: " << (double) hits_b / (double) N << std::endl;
-    std::cout << "Basic time: " << elapsed.count() << " ms" << std::endl;
-    std::cout << "Time per dot: " << elapsed.count() / N << " ms" << std::endl;
-
-    delete[] go_random;
     return 0;
 }
